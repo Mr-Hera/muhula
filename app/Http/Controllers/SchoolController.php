d5db639445ca9338a2616cac5d9c6cf3f8ecd9f6
Mr-Hera <randellhera@gmail.com>
2025-08-24 19:12:56 +0300

    Code cleanup

--- app/Http/Controllers/SchoolController.php
+++ app/Http/Controllers/SchoolController.php
@@ -957,7 +957,6 @@ class SchoolController extends Controller
         ->where('id', Auth::id())
         ->first();
     }
-    // dd($currentUserClaim);
 
     return view('search_school.school_details')->with([
       'school_record' => $school_record,
